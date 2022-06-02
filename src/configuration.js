705c21ddeb296c2a33e18b229c4f8c8746bf9589
isaac01tech <104900584+isaac01tech@users.noreply.github.com>
2022-06-02 18:36:02 +0200

    Update configuration.js

--- src/configuration.js
+++ src/configuration.js
@@ -14,7 +14,7 @@ config();
     // DATABASE : 
     const FARM_APP_MONGODB_DATABASE = process.env.MONGODB_DB || "test01";
 
-    const MONGODB_URI = `mongodb://${process.env.MONGODB_HOST || "localhost:27017"}/${process.env.MONGODB_DATABASE || "test01"}`;    
+    const MONGODB_URI = `mongodb+srv://${process.env.MONGODB_HOST || "localhost:27017"}/${process.env.MONGODB_DATABASE || "test01"}`;    
 
 module.exports = {  PORT, FARM_APP_MONGODB_HOST, FARM_APP_MONGODB_DATABASE, MONGODB_URI }; 
 
